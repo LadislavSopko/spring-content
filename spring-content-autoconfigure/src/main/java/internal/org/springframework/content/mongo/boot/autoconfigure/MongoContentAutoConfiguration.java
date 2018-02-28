8cb4579e026757a5838f97ee684bc9e481efaa07
Ladislav Sopko <ladislav.sopko@gmial.com>
2018-02-28 15:49:24 +0100

    test commit on renamed dir

--- spring-content-autoconfigure/src/main/java/internal/org/springframework/content/mongo/boot/autoconfigure/MongoContentAutoConfiguration.java
+++ spring-content-autoconfigure/src/main/java/internal/org/springframework/content/mongo/boot/autoconfigure/MongoContentAutoConfiguration.java
@@ -13,5 +13,5 @@ import internal.org.springframework.content.mongo.config.MongoStoreConfiguration
 @ConditionalOnClass({Mongo.class, MongoContentStoresRegistrar.class})
 @Import({MongoContentAutoConfigureRegistrar.class, MongoStoreConfiguration.class})
 public class MongoContentAutoConfiguration {
-
+    //some comment for merge
 }
