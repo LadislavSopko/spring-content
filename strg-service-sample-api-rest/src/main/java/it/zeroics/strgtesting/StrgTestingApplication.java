5148c02fadf8f0473ca1a6b5a62ecc8b112cb7a0
Ladislav Sopko <ladislav.sopko@gmial.com>
2018-03-16 16:28:27 +0100

    Finish first version of strg-service-sample-api-rest and strg-service-sample-api-shell

---
+++ strg-service-sample-api-rest/src/main/java/it/zeroics/strgtesting/StrgTestingApplication.java
@@ -0,0 +1,12 @@
+package it.zeroics.strgtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrgTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrgTestingApplication.class, args);
+	}
+}
