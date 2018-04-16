0390d07c31de843f92b607cb3c60384458a5f0e5
Ladislav Sopko <lsopko@3di.it>
2018-04-16 16:48:26 +0200

    Test succeed also with new/old strutture

--- strg-service-sample-api-rest/src/main/java/it/zeroics/strgtesting/StrgRestApplication.java
+++ strg-service-sample-api-rest/src/main/java/it/zeroics/strgtesting/StrgRestApplication.java
@@ -2,10 +2,10 @@ package it.zeroics.strgtesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.content.fs.config.EnableFilesystemStores;
+//import org.springframework.content.fs.config.EnableFilesystemStores;
 
 @SpringBootApplication
-@EnableFilesystemStores(basePackageClasses=it.zeroics.strg.api.rest.RestMediumContentStore.class)
+//@EnableFilesystemStores(basePackageClasses=it.zeroics.strg.api.rest.RestMediumContentStore.class)
 public class StrgRestApplication {
 
 	public static void main(String[] args) {
--- strg-service-sample-api-shell/src/main/java/it/zeroics/strgshell/StrgShellApplication.java
+++ strg-service-sample-api-shell/src/main/java/it/zeroics/strgshell/StrgShellApplication.java
@@ -2,10 +2,10 @@ package it.zeroics.strgshell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.content.fs.config.EnableFilesystemStores;
+//import org.springframework.content.fs.config.EnableFilesystemStores;
 
 @SpringBootApplication
-@EnableFilesystemStores(basePackageClasses=it.zeroics.strg.api.MediumContentStore.class)
+//@EnableFilesystemStores(basePackageClasses=it.zeroics.strg.api.MediumContentStore.class)
 public class StrgShellApplication {
 
 	public static void main(String[] args) {
